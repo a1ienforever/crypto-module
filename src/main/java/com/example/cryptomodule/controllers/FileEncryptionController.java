edc4adfb67cd9125bb05797ca23cc3c5602ed12f
alienforever <artyom.zadvornyj@yandex.ru>
2024-12-21 17:01:11 +0300

    refactor encrypt service

--- src/main/java/com/example/cryptomodule/controllers/FileEncryptionController.java
+++ src/main/java/com/example/cryptomodule/controllers/FileEncryptionController.java
@@ -10,19 +10,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.util.Pair;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.nio.file.Files;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.util.Base64;
+
+import com.example.cryptomodule.cryptography.EncryptionService;
 
 public class FileEncryptionController extends BaseController {
 
@@ -76,17 +68,21 @@ public class FileEncryptionController extends BaseController {
 
             String selectedEncryption = encryptionChoice.getValue();
             byte[] fileBytes = Files.readAllBytes(selectedFile.toPath());
+            EncryptionService encryptionService = new EncryptionService();
 
             byte[] encryptedData;
             switch (selectedEncryption) {
                 case "AES":
-                    encryptedData = encryptAES(fileBytes);
+                    encryptedData = encryptionService.encryptAES(fileBytes, keyField, ivField);
+                    System.gc();
                     break;
                 case "RSA":
-                    encryptedData = encryptRSA(fileBytes);
+                    encryptedData = encryptionService.encryptRSA(fileBytes, keyField);
+                    System.gc();
                     break;
                 case "DES":
-                    encryptedData = encryptDES(fileBytes);
+                    encryptedData = encryptionService.encryptDES(fileBytes, keyField);
+                    System.gc();
                     break;
                 default:
                     throw new Exception("Invalid encryption method");
@@ -113,17 +109,21 @@ public class FileEncryptionController extends BaseController {
 
             String selectedEncryption = encryptionChoice.getValue();
             byte[] fileBytes = Files.readAllBytes(selectedFile.toPath());
+            EncryptionService encryptionService = new EncryptionService();
 
             byte[] decryptedData;
             switch (selectedEncryption) {
                 case "AES":
-                    decryptedData = decryptAES(fileBytes);
+                    decryptedData = encryptionService.decryptAES(fileBytes, keyField, ivField);
+                    System.gc();
                     break;
                 case "RSA":
-                    decryptedData = decryptRSA(fileBytes);
+                    decryptedData = encryptionService.decryptRSA(fileBytes, keyField);
+                    System.gc();
                     break;
                 case "DES":
-                    decryptedData = decryptDES(fileBytes);
+                    decryptedData = encryptionService.decryptDES(fileBytes, keyField);
+                    System.gc();
                     break;
                 default:
                     throw new Exception("Invalid decryption method");
@@ -140,88 +140,6 @@ public class FileEncryptionController extends BaseController {
         }
     }
 
-    // AES Encryption
-    private byte[] encryptAES(byte[] data) throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(128);
-        SecretKey secretKey = keyGenerator.generateKey();
-
-        byte[] iv = new byte[16];
-        new java.security.SecureRandom().nextBytes(iv);
-        IvParameterSpec ivSpec = new IvParameterSpec(iv);
-
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
-
-        keyField.setText(Base64.getEncoder().encodeToString(secretKey.getEncoded()));
-        ivField.setText(Base64.getEncoder().encodeToString(iv));
-
-        return cipher.doFinal(data);
-    }
-
-    // AES Decryption
-    private byte[] decryptAES(byte[] data) throws Exception {
-        byte[] keyBytes = Base64.getDecoder().decode(keyField.getText());
-        byte[] ivBytes = Base64.getDecoder().decode(ivField.getText());
-
-        SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
-        IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
-
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
-
-        return cipher.doFinal(data);
-    }
-
-    // RSA Encryption
-    private byte[] encryptRSA(byte[] data) throws Exception {
-        Pair<String, String> keys = KeyGeneratorService.generateKeyPair();
-        String privateKey = keys.getKey();
-        String publicCodedKey = keys.getValue();
-        keyField.setText(privateKey);
-
-        byte[] decodedKey = Base64.getDecoder().decode(publicCodedKey);
-        PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new java.security.spec.X509EncodedKeySpec(decodedKey));
-
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-
-        return cipher.doFinal(data);
-    }
-
-    // RSA Decryption
-    private byte[] decryptRSA(byte[] data) throws Exception {
-        byte[] privateKeyBytes = Base64.getDecoder().decode(keyField.getText());
-        PrivateKey privateKey = KeyFactory.getInstance("RSA").generatePrivate(new java.security.spec.PKCS8EncodedKeySpec(privateKeyBytes));
-
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-
-        return cipher.doFinal(data);
-    }
-
-    // DES Encryption
-    private byte[] encryptDES(byte[] data) throws Exception {
-        SecretKeySpec secretKey = new SecretKeySpec("12345678".getBytes(), "DES");
-
-        Cipher cipher = Cipher.getInstance("DES");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-
-        keyField.setText(Base64.getEncoder().encodeToString(secretKey.getEncoded()));
-
-        return cipher.doFinal(data);
-    }
-
-    // DES Decryption
-    private byte[] decryptDES(byte[] data) throws Exception {
-        byte[] keyBytes = Base64.getDecoder().decode(keyField.getText());
-        SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "DES");
-
-        Cipher cipher = Cipher.getInstance("DES");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
-        return cipher.doFinal(data);
-    }
 
     private void showAlert(Alert.AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
