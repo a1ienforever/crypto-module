e80a0eaef2619a5d74ad0e13af2dd74ee6c3bda7
alienforever <artyom.zadvornyj@yandex.ru>
2024-12-22 16:22:55 +0300

    crypt container

--- src/main/java/com/example/cryptomodule/controllers/CryptContainerController.java
+++ src/main/java/com/example/cryptomodule/controllers/CryptContainerController.java
@@ -76,13 +76,7 @@ public class CryptContainerController extends BaseController {
         }
     }
 
-    private void showAlert(Alert.AlertType alertType, String title, String message) {
-        Alert alert = new Alert(alertType);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.showAndWait();
-    }
+
 
     @FXML
     private void goBack() {
